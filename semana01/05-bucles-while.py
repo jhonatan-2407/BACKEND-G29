d51fa15214839063009abc3a2048a28ed44e7a16
Jhonatan Rivera <jp.rivera.al@gmail.com>
2026-03-13 20:42:56 -0500

    prueba 01

---
+++ semana01/05-bucles-while.py
@@ -0,0 +1,14 @@
+numero = 20
+
+while numero > 0:
+    print (numero)
+    # numero = numero - 1
+    numero -= 3
+
+# Todos los valores ingresados siempre seran string
+variable = input("Ingresa un numero: ")
+print(type(variable))
+#para convertir a numero
+variable_numerica = int(variable)
+print(type(variable_numerica))
+print(variable)
---
+++ semana01/06-ejercicio-while.py
@@ -0,0 +1,22 @@
+#Se tiene numero ganador y el ususario tiene que intentar adivinarlo, si el numero ganador es 10
+#y  el usuario ingresa 20, entonces indicarle que el numero es menor
+# y si el usuario ingresa 5, indicarles que el numero es mayor
+# si el usuario adivina el numero, felicitarlo y termina el juego
+
+numero_ganador = 10
+intento = 0
+
+while intento != numero_ganador:
+    
+   
+    intento = int(input("Introduce un número: "))
+    
+    if intento > numero_ganador:
+        print("El número secreto es menor")
+    elif intento < numero_ganador:
+        print("El número secreto es mayor")
+    else:
+        print("Felicidades encontraste el número secreto")
+
+print("Fin del juego")
+    
